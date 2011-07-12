8c9a397b734c4e33b9a9cde4352e0620a00d7152
Gaius <github@gaius.org.uk>
2011-07-12 11:07:03 +0100

    initial commit

--- extproc_add.c
+++ extproc_add.c
@@ -1,4 +1,17 @@
 /* Example of implementing an EXTPROC in OCaml - C stub */
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <caml/mlvalues.h>
+#include <caml/callback.h>
+
+int initialized = FALSE;
+
+int add_two(int x, int y) {
+  if (!initialized) {
+    caml_startup();
+  }
+}
+
 /* End of file */
